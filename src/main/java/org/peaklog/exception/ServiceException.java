d83d691cb46751e734da1a66c9eecc6a1bc2fa20
joseacarmo17 <joseantoniocarmo17@gmail.com>
2026-02-28 01:22:41 +0100

    Exception service

---
+++ src/main/java/org/peaklog/exception/ServiceException.java
@@ -0,0 +1,20 @@
+package org.peaklog.exception;
+
+public class ServiceException extends RuntimeException {
+
+    private final ErrorCode errorCode;
+
+    public ServiceException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+
+    public ServiceException(ErrorCode errorCode, String customMessage) {
+        super(customMessage);
+        this.errorCode = errorCode;
+    }
+
+    public ErrorCode getErrorCode() {
+        return errorCode;
+    }
+}
